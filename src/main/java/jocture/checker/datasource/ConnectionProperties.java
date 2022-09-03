d49d9b5848836ed8fbf2a50938424ffc3ed1bbde
tntis <tnbinsu@gmail.com>
2022-09-03 10:21:48 +0900

    checker 2번쨰

--- src/main/java/jocture/checker/datasource/ConnectionProperties.java
+++ src/main/java/jocture/checker/datasource/ConnectionProperties.java
@@ -3,6 +3,7 @@ package jocture.checker.datasource;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.util.StringUtils;
 
 @Setter @Getter
 @ToString
@@ -11,4 +12,13 @@ public class ConnectionProperties {
     private String url;
     private String username;
     private String password;
+
+    boolean validate(){
+        return StringUtils.hasText(url) && StringUtils.hasText(username);
+    }
 }
+
+
+
+
+// 1급 컬렉션
--- src/main/java/jocture/checker/datasource/DataSourceConnectionChecker.java
+++ src/main/java/jocture/checker/datasource/DataSourceConnectionChecker.java
@@ -27,24 +27,69 @@ public class DataSourceConnectionChecker implements ExitCodeGenerator {
 
     @EventListener(ApplicationReadyEvent.class)
     public void checkConnections(){
-        List<ConnectionProperties> list = dataSourceProperties.getList();
-        list.forEach(prop-> {
-            log.debug("prop = {}",prop);
-            // 커넥션 생성
-            // 커넥션 : 실제 물리 DB와 연결 세션(DataSource 를 통해 커넥션을 획득한다.
-            // 커넥션 연결 방법 : DataSource룰 통해서 한다.
-            DataSource datasource = new DriverManagerDataSource(prop.getUrl(), prop.getUsername(), prop.getPassword());
-            try (Connection conn = datasource.getConnection()) {
-                log.info("SUCCESS : {} ({})", prop.getName(),prop.getUrl());
-            }catch (Exception e){
-                log.error("FAIL : {} ({})", prop.getName(),prop.getUrl());
-                exitCode = 1;
-            }
+        List<ConnectionProperties> propList = dataSourceProperties.getList();
 
-        });
+        long successCount = propList.stream()
+            .filter(this::checkConnection)
+            .count();
+
+        setExitCodeToErrorIfNotAllSuccess(successCount);
+        log.info("Connection Check End : {}/{} (success/total)", successCount,propList.size());
     }
 
+    private boolean checkConnection(ConnectionProperties prop) {
+        log.debug("conn ={}",prop);
+       if(!prop.validate()){
+           return failBecauseOfInvalidProperty(prop);
+       }
+        return establishConnection(prop);
+    }
+
+    private boolean failBecauseOfInvalidProperty(ConnectionProperties prop) {
+        log.error("FAIL : {} ({})", prop.getName(), prop.getUrl());
+        return false;
+    }
+
+    private boolean establishConnection(ConnectionProperties prop) {
+        // 예전 사용 방식 : 구성(configuration)과 사용이 분리되지 않는 형태로 좋지 않은 설계이다
+        //Connection conn1 = DriverManager.getConnection(prop.getUrl(),prop.getUsername(), prop.getPassword());
+
+        // 커넥션 생성
+        // 커넥션 : 실제 물리 DB와 연결 세션(DataSource 를 통해 커넥션을 획득한다.
+        // 커넥션 연결 방법 : DataSource룰 통해서 한다.
+
+        // DataSource 인터페이스를 사용하면, 구성과 사용이 분리된다
+        DataSource datasource = new DriverManagerDataSource(prop.getUrl(), prop.getUsername(), prop.getPassword());
+        try (Connection conn = datasource.getConnection()) {
+            log.info("SUCCESS : {} ({})", prop.getName(), prop.getUrl());
+            return true;
+        }catch (Exception e){
+            log.error("FAIL : {} ({})", prop.getName(), prop.getUrl());
+            return false;
+        }
+    }
+
+    private void setExitCodeToErrorIfNotAllSuccess(long successCount) {
+        if(successCount < dataSourceProperties.getList().size()){
+            exitCode =1;
+        }
+    }
 }
+
+
+
 // 커넥션
 //
 // 결과값
+
+
+
+// 스트림 : 메서드(연산자) 체인을 사용
+// 연산자 종류
+// 1) 종단 연산자 : forEach
+// 2) 중간 연산자 : filter, map
+// 종단 연산자로 끝나야지만 중간 연산자가 돌아간다
+
+
+//forEach : 스트림 종단(Terminal) 연산자
+// 중간(Intermediate) 연산자
